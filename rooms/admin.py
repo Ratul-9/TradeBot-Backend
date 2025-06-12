99f5f06b91fda300a6e127fe7884983ec283b466
Ratul-9 <mukherjeeratul2023@gmail.com>
2025-06-12 11:58:28 +0530

    Added Rooms Models

--- rooms/admin.py
+++ rooms/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Room, RoomParticipant, UserBalance, Trade
 
-# Register your models here.
+admin.site.register(Room)
+admin.site.register(RoomParticipant)
+admin.site.register(UserBalance)
+admin.site.register(Trade)
