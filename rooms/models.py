d78462061d22bc19ae6198c97ba56ecc3bccea0e
Ratul-9 <mukherjeeratul2023@gmail.com>
2025-06-12 19:23:30 +0530

    Made staff only to create rooms

--- rooms/models.py
+++ rooms/models.py
@@ -9,6 +9,7 @@ class Room(models.Model):
     password = models.CharField(max_length=50, blank=True, null=True)
     start_time = models.DateTimeField(null=True, blank=True)
     end_time = models.DateTimeField(null=True, blank=True)
+    admin = models.ForeignKey(User, on_delete=models.CASCADE, related_name='created_rooms')
 
     def __str__(self):
         return f"Room: {self.name} (Admin: {self.admin.username})"
