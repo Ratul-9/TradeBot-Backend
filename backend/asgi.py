cb56d5a49db959677312e8f239351add207ac414
Ratul-9 <mukherjeeratul2023@gmail.com>
2025-07-03 12:13:47 +0530

    Changes Made to deploy config

--- backend/asgi.py
+++ backend/asgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
+import django
+from channels.routing import get_default_application
 
-from django.core.asgi import get_asgi_application
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "your_project_name.settings")
+django.setup()
+application = get_default_application()
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
-
-application = get_asgi_application()
