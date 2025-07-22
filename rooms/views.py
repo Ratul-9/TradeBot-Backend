c763e9c8d484d1d6837eb5f529c8ad8ed0e5f502
Ratul-9 <mukherjeeratul2023@gmail.com>
2025-07-22 19:23:13 +0530

    Fixing Historical Data API

--- rooms/views.py
+++ rooms/views.py
@@ -933,6 +933,14 @@ class HistoricalDataView(APIView):
             # === Upstox SDK ===
             historical_api = upstox_client.HistoryV3Api()
 
+            return Response({
+                "instruement_key": instrument_key,
+                "interval": interval,
+                "unit": unit,
+                "from": from_date,
+                "to": to_date
+            })
+
             try:
                 response = historical_api.get_historical_candle_data1(
                     instrument_key=instrument_key,
