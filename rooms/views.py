c8d175216c35a4bea4a82a38c3f55a5d1efc7415
Ratul-9 <mukherjeeratul2023@gmail.com>
2025-07-22 19:35:47 +0530

    Fixing Historical Data API

--- rooms/views.py
+++ rooms/views.py
@@ -950,7 +950,9 @@ class HistoricalDataView(APIView):
                     to_date=to_date
                 )
 
-                return Response({"data": response.to_dict()})
+                candles = response.data.candles
+
+                return Response({candles})
 
                 candles_data = getattr(response, "data", {}).get("candles", [])
                 if not candles_data:
