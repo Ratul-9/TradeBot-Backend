1f693a6074d48104cc06dbb2f7eef8961c031171
Ratul-9 <mukherjeeratul2023@gmail.com>
2025-08-26 21:56:57 +0530

    Trying auto Square off logic Fixing issues - 3

--- rooms/tasks.py
+++ rooms/tasks.py
@@ -15,6 +15,8 @@ logger  = logging.getLogger(__name__)
 
 @shared_task
 def execute_pending_orders():
+    logger.warning("Running execute_pending_orders at %s", timezone.now())
+
     symbols = MarketData.objects.values_list('symbol', flat=True).distinct()
 
     for symbol in symbols:
